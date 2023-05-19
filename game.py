7f0a577653e6eeff463c9ac9d658676f8ebcf2e3
salma-mohamed-37 <110353312+salma-mohamed-37@users.noreply.github.com>
2023-05-19 15:59:58 +0300

    add will win function

--- game.py
+++ game.py
@@ -5,6 +5,28 @@ import random
 # GAME LINK
 # http://kevinshannon.com/connect4/
 
+def willWin(GameBoard,player):
+    for j in range(cols-2):#horizontal
+        for i in range (rows):
+            if GameBoard[i][j]== player and GameBoard[i][j+1]== player and GameBoard[i][j+2]==player:
+                return -100
+            
+    for j in range(cols): #vertical
+        for i in range (rows-2): 
+            if GameBoard[i][j]== player and GameBoard[i+1][j]== player and GameBoard[i+2][j]==player:
+                return -500
+            
+    for j in range(cols-2): #top left
+        for i in range (rows-2): 
+            if GameBoard[i][j]== player and GameBoard[i+1][j+1]== player and GameBoard[i+2][j+2]==player:
+                return -500
+            
+    for j in range(cols-2):  #top right
+        for i in range (2,rows):
+            if GameBoard[i][j]== player and GameBoard[i-1][j+1]== player and GameBoard[i-2][j+2]==player :
+                return -500
+    return 0        
+
 
 def main():
     board = Board()
